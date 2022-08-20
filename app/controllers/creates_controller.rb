a06b54f22b9fd2b803016e413f7dd22866903c7b
SangeethaRamachanthiran <sangeetha.r1409@gmail.com>
2022-08-20 14:19:08 +0530

    fix some bugs

--- app/controllers/creates_controller.rb
+++ app/controllers/creates_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CreatesController < ApplicationController
-  @@test_id = 0
+  @@test_id = 1
   def quiz
     @users_ques = @current_user.id
     @side_ques = Quiz.find_by(users_id: @users_ques)
@@ -56,7 +56,7 @@ class CreatesController < ApplicationController
   end
 
   def increase_test_id
-    @@test_id += 1
+    # @@test_id += 1
     p '=================================================='
     redirect_to action: 'add_library', save: heads_params
   end
